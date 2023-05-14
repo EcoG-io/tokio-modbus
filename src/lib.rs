e8c90ef125bfc976f255eab8083298ba199d2c96
Johannes Hund <johannes.hund@gmail.com>
2023-05-14 20:41:07 +0200

    allow access to error responses

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,9 @@ pub use self::slave::{Slave, SlaveId};
 mod codec;
 
 mod frame;
-pub use self::frame::{Address, FunctionCode, Quantity, Request, Response};
+pub use self::frame::{
+    Address, Exception, ExceptionResponse, FunctionCode, Quantity, Request, Response, ResponsePdu,
+};
 
 mod service;
 
